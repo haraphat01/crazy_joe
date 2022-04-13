db9531baaac23bc77834bd921a11a48b02d0ca9e
haraphat01 <haraphat@gmail.com>
2022-04-13 15:01:15 +0100

    Added the link to create new customer, sales and game and fixed the migration issue

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -65,6 +65,6 @@ class GamesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def game_params
-      params.require(:game).permit(:name, :age, :stock, :price, :image)
+      params.require(:game).permit(:name, :age, :stock, :price, :image, :customer_idq)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_03_221714) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_13_135912) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,7 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_03_221714) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "game_id"
   end
 
   create_table "games", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
